4814591067008f78c29766601813b89d232ad070
Mohamed Ali <a74498722@gmail.com>
2025-12-07 22:36:50 -0800

    Refactor: Abstract Firestore behind DatabaseService
    Update the service locator to register FireStoreService as a DatabaseService interface. The ProductRepo is also updated to depend on this new abstraction instead of the concrete implementation.
    
    This change decouples the repository layer from a specific database technology, improving modularity and making the system easier to test and maintain.

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -2,6 +2,7 @@ import 'package:fruits_hub_dashboard/core/repos/images_repo/images_repo.dart';
 import 'package:fruits_hub_dashboard/core/repos/images_repo/images_repo_implement.dart';
 import 'package:fruits_hub_dashboard/core/repos/product_repo/product_repo.dart';
 import 'package:fruits_hub_dashboard/core/repos/product_repo/products_repo_implement.dart';
+import 'package:fruits_hub_dashboard/core/services/database_service.dart';
 import 'package:fruits_hub_dashboard/core/services/fire_storage.dart';
 import 'package:fruits_hub_dashboard/core/services/firestore_service.dart';
 import 'package:fruits_hub_dashboard/core/services/storag_service.dart';
@@ -18,10 +19,10 @@ void setupGetIt() {
       getIt<StorageService>(),
     ),
   );
-  getIt.registerSingleton<FireStoreService>(
+  getIt.registerSingleton<DatabaseService>(
     FireStoreService(),
   );
   getIt.registerSingleton<ProductRepo>(
-    ProductsRepoImplement(getIt<FireStoreService>()),
+    ProductsRepoImplement(getIt<DatabaseService>()),
   );
 }
