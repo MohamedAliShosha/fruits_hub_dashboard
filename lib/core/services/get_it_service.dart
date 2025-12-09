f6e21760593b920a97588833ddd0e214919e11ef
Mohamed Ali <a74498722@gmail.com>
2025-12-09 13:27:07 -0800

    feat: Implement Supabase storage and switch from Firebase
    Implement the `uploadFile` method in `SupabaseStorageService` to handle file uploads to the 'fruits_images' bucket on Supabase.
    
    Update the GetIt service locator to register `SupabaseStorageService` as the active `StorageService`, replacing the previous `FireStorage` implementation. This completes the migration of the storage service to Supabase.

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -3,16 +3,16 @@ import 'package:fruits_hub_dashboard/core/repos/images_repo/images_repo_implemen
 import 'package:fruits_hub_dashboard/core/repos/product_repo/product_repo.dart';
 import 'package:fruits_hub_dashboard/core/repos/product_repo/products_repo_implement.dart';
 import 'package:fruits_hub_dashboard/core/services/database_service.dart';
-import 'package:fruits_hub_dashboard/core/services/fire_storage.dart';
 import 'package:fruits_hub_dashboard/core/services/firestore_service.dart';
 import 'package:fruits_hub_dashboard/core/services/storag_service.dart';
+import 'package:fruits_hub_dashboard/core/services/supabase_storage.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
   getIt.registerSingleton<StorageService>(
-    FireStorage(),
+    SupabaseStorageService(),
   );
   getIt.registerSingleton<ImagesRepo>(
     ImagesRepoImplement(
--- lib/core/services/supabase_storage.dart
+++ lib/core/services/supabase_storage.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:fruits_hub_dashboard/core/services/storag_service.dart';
 import 'package:fruits_hub_dashboard/core/utils/constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:path/path.dart' as b;
 
 class SupabaseStorageService implements StorageService {
   static late Supabase supabase;
@@ -16,5 +17,12 @@ class SupabaseStorageService implements StorageService {
   }
 
   @override
-  Future<String> uploadFile(File file, String path) {}
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = file.path;
+    String extensionName = b.extension(file.path);
+    var result = await supabase.client.storage
+        .from('fruits_images')
+        .upload('$path/$fileName.$extensionName', file);
+    return result;
+  }
 }
