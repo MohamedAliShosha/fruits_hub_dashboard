a2abdcda4f07a6bfeaa6da3df521df4915270daf
Mohamed Ali <160479090+MohamedAliShosha@users.noreply.github.com>
2026-01-06 16:13:02 -0800

    feat(routes): Add route handling for OrdersView

--- lib/core/functions/on_generate_route.dart
+++ lib/core/functions/on_generate_route.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_dashboard/features/add_product/presentation/views/add_product_view.dart';
 import 'package:fruits_hub_dashboard/features/dashboard/views/dashboard_view.dart';
+import 'package:fruits_hub_dashboard/features/orders/presentation/views/orders_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   return MaterialPageRoute(builder: (_) {
     switch (settings.name) {
       case DashboardView.routeName:
         return const DashboardView();
+      case OrdersView.routeName:
+        return const OrdersView();
       case AddProductView.routeName:
         return const AddProductView();
 
