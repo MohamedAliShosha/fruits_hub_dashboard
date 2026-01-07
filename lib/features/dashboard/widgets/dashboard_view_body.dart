fee32d371d7a2ab215b5350c0d6ec25820cb6ff5
Mohamed Ali <160479090+MohamedAliShosha@users.noreply.github.com>
2026-01-06 16:14:34 -0800

    feat(dashboard): Add button to navigate to OrdersView

--- lib/features/dashboard/widgets/dashboard_view_body.dart
+++ lib/features/dashboard/widgets/dashboard_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_dashboard/core/widgets/app_button.dart';
 import 'package:fruits_hub_dashboard/features/add_product/presentation/views/add_product_view.dart';
+import 'package:fruits_hub_dashboard/features/orders/presentation/views/orders_view.dart';
 
 class DashboardViewBody extends StatelessWidget {
   const DashboardViewBody({super.key});
@@ -17,6 +18,14 @@ class DashboardViewBody extends StatelessWidget {
             onPressed: () {
               // Navigate to add data view
               Navigator.pushNamed(context, AddProductView.routeName);
+            },
+          ),
+          const SizedBox(height: 16),
+          AppButton(
+            text: 'View Orders',
+            onPressed: () {
+              // Navigate to add data view
+              Navigator.pushNamed(context, OrdersView.routeName);
             },
           ),
         ],
