582f1e67d7af8cfa328802abbc2beef3abdc3ee6
Mohamed Ali <160479090+MohamedAliShosha@users.noreply.github.com>
2026-01-08 17:09:54 -0800

    feat(fetch_orders_cubit): refactor fetchOrders to use Stream for real-time updates

--- lib/features/orders/presentation/manager/fetch_orders/fetch_orders_cubit.dart
+++ lib/features/orders/presentation/manager/fetch_orders/fetch_orders_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub_dashboard/features/orders/domain/entities/order_entity.dart';
 import 'package:fruits_hub_dashboard/features/orders/domain/repos/orders_repo.dart';
@@ -8,20 +10,35 @@ class FetchOrdersCubit extends Cubit<FetchOrdersState> {
   FetchOrdersCubit(this.ordersRepo) : super(FetchOrdersInitial());
 
   final OrdersRepo ordersRepo;
-  Future<void> fetchOrders() async {
+
+  // StreamSubscription is a reference to a stream to enable us to cancel the stream
+  StreamSubscription? _streamSubscription;
+
+  void fetchOrders() {
     emit(FetchOrdersLoading());
-    final result = await ordersRepo.fetchOrders();
-    result.fold(
-      (failure) => emit(
-        FetchOrdersFailure(
-          failure.errorMessage,
-        ),
-      ),
-      (orders) => emit(
-        FetchOrdersSuccess(
-          orders,
-        ),
-      ),
+
+    _streamSubscription = ordersRepo.fetchOrders().listen(
+      (result) {
+        result.fold(
+          (failure) => emit(
+            FetchOrdersFailure(
+              failure.errorMessage,
+            ),
+          ),
+          (orders) => emit(
+            FetchOrdersSuccess(
+              orders,
+            ),
+          ),
+        );
+      },
     );
+  }
+
+  // Cancel the stream subscription when the cubit is closed
+  @override
+  Future<void> close() async {
+    await _streamSubscription?.cancel();
+    return super.close();
   }
 }
