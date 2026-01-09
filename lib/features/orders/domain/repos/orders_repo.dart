87d302350863bce4739fc0d186aa767840e7d612
Mohamed Ali <160479090+MohamedAliShosha@users.noreply.github.com>
2026-01-08 17:09:46 -0800

    feat(orders_repo): Change fetchOrders to return a Stream for real-time updates

--- lib/features/orders/domain/repos/orders_repo.dart
+++ lib/features/orders/domain/repos/orders_repo.dart
@@ -3,5 +3,6 @@ import 'package:fruits_hub_dashboard/core/errors/failures.dart';
 import 'package:fruits_hub_dashboard/features/orders/domain/entities/order_entity.dart';
 
 abstract class OrdersRepo {
-  Future<Either<Failure, List<OrderEntity>>> fetchOrders();
+  // I changed from future to stream to get real time updates
+  Stream<Either<Failure, List<OrderEntity>>> fetchOrders();
 }
